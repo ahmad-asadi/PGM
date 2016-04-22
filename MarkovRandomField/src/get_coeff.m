64ab62765398c3077b6e77668c1b80c9083c18fc
Ahmad Asadi <asadi@rapna.net>
2016-04-22 11:42:06 +0430

    last commit before solving graphisc issue

---
+++ MarkovRandomField/src/get_coeff.m
@@ -0,0 +1,19 @@
+function coeff = get_coeff(W,x,y)
+coeff = -1 ;
+max_i = size(W,1);
+max_j = size(W,2);
+for i = - 1 : 1
+    for j = -1 : 1 
+        if(is_in_bound(i,j,max_i, max_j,x,y))
+           if(W(x + i, y + j) == W(x,y)) 
+               coeff = coeff + 1 ;               
+           else
+               coeff = coeff - 1 ;
+           end
+        end
+    end
+end
+
+
+function res = is_in_bound(i,j,max_i,max_j , x ,y)
+res = i > -x && i < max_i - x && j > -y && j < max_j - y ; 
---
+++ MarkovRandomField/src/naive_bayes_segmentation.m
@@ -0,0 +1,91 @@
+%Segmentation using naive bayes classifier
+
+disp('loading image');
+simage = imread('../test1.bmp');
+
+simage = simage(:,:,1);
+orig_image = simage;
+
+noise_mean = 0 ;
+noise_variance = 0.5 ;
+
+g0 = 0 ;
+g1 = 0 ;
+g2 = 0 ;
+
+for i = 1 : 417
+    for j = 1:415
+        if(simage(i,j) == 0)
+            g0 = g0 +1 ;
+        elseif(simage(i,j)==127)
+            g1 = g1 + 1 ;
+        else
+            g2 = g2 + 1 ;
+        end
+    end
+end
+
+sg = g0+g1+g2 ;
+
+g0 = g0/sg ;
+g1 = g1/sg ;
+g2 = g2/sg ;
+
+
+simage = imnoise(simage,'gaussian', noise_mean,noise_variance);
+figure;
+title('noisy_input');
+image = double(simage);
+
+disp('adding gaussian noise with mean = 0 and var= 0.01 to the image');
+noisy_image = image;
+% noisy_image = imnoise(noisy_image, 'gaussian' , noise_mean , noise_variance);
+
+% imshow(mat2gray(noisy_image));
+% figure;
+
+mu0 = 0 ;
+mu1 = 127 ;
+mu2 = 200 ;
+
+sigma = 64 ;
+
+posteriors = zeros(417, 415, 3) ;
+
+
+for i = 1 : size(noisy_image,1)
+    fprintf('calculating posterior probs for i: %d\n' , i);
+    for j = 1 : size(noisy_image,2)
+        posteriors(i,j,1) = normpdf(noisy_image(i,j),mu0,sigma) * g0;
+        posteriors(i,j,2) = normpdf(noisy_image(i,j),mu1,sigma) * g1;
+        posteriors(i,j,3) = normpdf(noisy_image(i,j),mu2,sigma) * g2;
+    end
+end
+
+res = zeros(size(posteriors,1),size(posteriors,2)) - 1;
+for i = 1 : size(posteriors,1)
+    fprintf('calculating MLE for i: %d\n' , i);
+    for j = 1 : size(posteriors,2)
+        [~,res(i,j)] = max(posteriors(i,j,:));
+        if(res(i,j)==1)
+            res(i,j) = 0;
+        elseif(res(i,j)==2)
+            res(i,j) = 127;
+        elseif(res(i,j)==3)
+            res(i,j) = 255;
+        end
+    end
+end
+
+figure;
+title('final_result');
+imshow(mat2gray(res));
+
+
+
+figure;
+title('difference');
+imshow(mat2gray(double(orig_image) - res*255/3));
+
+
+
